346b5a5e355017a1d109b2f7bcd3c02fd234c54f
BrunoNoberto <bruno.norberto@konicaminolta.pt>
2024-04-23 12:58:03 +0100

    aula: operador condicional

--- Program.cs
+++ Program.cs
@@ -45,24 +45,101 @@
 // 3 - Convertendo valores (Casting)
 // A diferenca de Parse para convert esta no tratamento de valores nulos
 
-int a = Convert.ToInt32("5"); // ToInt32 pq int tem 32 bits. Aqui o 5 ira virar inteiro
-int b = int.Parse("5"); // tem a mesma funcionalidade que a linha acima
-int aNulo = Convert.ToInt32(null); // vai dar 0
-// int bNulo = int.Parse(null); // da erro pq o valor nao pode ser nulo
-// int c = int.Parse("5c"); //  vai dar um erro pois a string contem letra e letra nao pode virar numero inteiro
-int inteiro = 6;
-string d = inteiro.ToString();
+// int a = Convert.ToInt32("5"); // ToInt32 pq int tem 32 bits. Aqui o 5 ira virar inteiro
+// int b = int.Parse("5"); // tem a mesma funcionalidade que a linha acima
+// int aNulo = Convert.ToInt32(null); // vai dar 0
+// // int bNulo = int.Parse(null); // da erro pq o valor nao pode ser nulo
+// // int c = int.Parse("5c"); //  vai dar um erro pois a string contem letra e letra nao pode virar numero inteiro
+// int inteiro = 6;
+// string d = inteiro.ToString();
 
-Console.WriteLine($"Valor da variavel a: {a}");
-Console.WriteLine($"Valor da variavel b: {b}");
-Console.WriteLine($"Valor da variavel aNulo: {aNulo}");
-// Console.WriteLine($"Valor da variavel bNulo: {bNulo}");
-// Console.WriteLine($"Valor da variavel c: {c}");
-Console.WriteLine($"Valor da variavel d: {d}");
+// Console.WriteLine($"Valor da variavel a: {a}");
+// Console.WriteLine($"Valor da variavel b: {b}");
+// Console.WriteLine($"Valor da variavel aNulo: {aNulo}");
+// // Console.WriteLine($"Valor da variavel bNulo: {bNulo}");
+// // Console.WriteLine($"Valor da variavel c: {c}");
+// Console.WriteLine($"Valor da variavel d: {d}");
 
 
-string txt = "15-";
-int x = 0;
+// string txt = "15-";
+// int x = 0;
 
-// VAi pegar o valor de entrada (txt) e tentar converter o valor e adicionar no valor de saida (out x). Caso nao de certo, vai imprimir o valor default da var de saida
-int.TryParse(txt, out x);
+// // VAi pegar o valor de entrada (txt) e tentar converter o valor e adicionar no valor de saida (out x). Caso nao de certo, vai imprimir o valor default da var de saida
+// int.TryParse(txt, out x);
+
+
+
+// ==================================================================================================================
+
+
+
+// 4 - Operadores condicionais
+
+// int quantidadeEstoque = 10;
+// int quantidadeCompra = 4;
+
+// // Se o valor for true executa o if se for false executa o else
+// if(quantidadeEstoque >= quantidadeCompra)
+// {
+//     Console.WriteLine("Compra realizada!");
+// }
+// else
+// {
+//     Console.WriteLine("Nao tem a quantidade desejada em estoque!");
+// }
+
+// // if aninhado
+// if(quantidadeCompra == 0)
+// {
+//     Console.WriteLine("Venda invalida");
+// }
+// else if (quantidadeEstoque >= quantidadeCompra)
+// {
+//     Console.WriteLine("Compra realizada!");
+// }
+// else
+// {
+//     Console.WriteLine("Nao tem a quantidade desejada em estoque!");
+// }
+
+Console.WriteLine("Digite uma letra:");
+string letra = Console.ReadLine(); // vai ler o que for digitado no console e armazenar na variavel
+
+// verificar se a letra e vogal ou nao
+switch (letra)
+{
+    case "a":
+        Console.WriteLine("vogal");
+        break;
+    case "e":
+        Console.WriteLine("vogal");
+        break;
+    case "i":
+        Console.WriteLine("vogal");
+        break;
+    case "o":
+        Console.WriteLine("vogal");
+        break;
+    case "u":
+        Console.WriteLine("vogal");
+        break;
+    default:
+        Console.WriteLine("Nao e uma vogal");
+        break;
+}
+
+// ou
+
+switch (letra)
+{
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        Console.WriteLine("vogal");
+        break;
+    default:
+        Console.WriteLine("Nao e uma vogal");
+        break;
+}
